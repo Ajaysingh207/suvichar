78633f006eda2350fb32ab6356cb71a9e0a37b60
Ajaysingh787898 <ajay.singh@codeviksolutions.com>
2025-12-04 00:43:26 +0530

    this is

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use("/uploads", express.static("uploads"));
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
@@ -34,7 +34,7 @@ app.use("/", (req, res) => {
 // Attach Socket.io Server
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "*",
         methods: ["GET", "POST"],
         credentials: true
     }
